2e37f10241730831d0d5a24e472364165148871f
Nicolas <nicopuegher@gmail.com>
2026-03-04 18:22:28 +0000

    Define the __name__ of the TestTextNode to run when called directly

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,3 +8,7 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
