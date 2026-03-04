d3afbe79edb75110b3acac6e884922824a3e79e8
Nicolas <nicopuegher@gmail.com>
2026-03-04 18:23:26 +0000

    Remove TextType enum value inside the constructor in textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -13,7 +13,7 @@ class TextType(Enum):
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = TextType[text_type]
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
