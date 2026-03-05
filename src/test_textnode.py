098559a7541fbc50b78c5e198954fd4a20ad0d95
Nicolas <nicopuegher@gmail.com>
2026-03-05 19:56:54 +0000

    Define a unit test for different types for TextNodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a different node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
+    def test_type_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
