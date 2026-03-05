d97018d8d40d6c543ba0e908cc5cdcb7615d8757
Nicolas <nicopuegher@gmail.com>
2026-03-05 21:09:59 +0000

    Define a unit test for different tags for HTMLNodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,6 +9,11 @@ class TestHTMLNode(unittest.TestCase):
         node2 = HTMLNode("h1", "Hello, World!", [], {"class": "title"})
         self.assertEqual(node, node2)
 
+    def test_tag_eq(self):
+        node = HTMLNode("h1", "Hello, World!", [], {"class": "title"})
+        node2 = HTMLNode("h2", "Hello, World!", [], {"class": "title"})
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
