fae660ca5c7e169dcc6b50bfb352c76f5c7a2561
Nicolas <nicopuegher@gmail.com>
2026-03-10 15:05:02 +0000

    Define a to_html method inside LeafNode class

--- src/leafnode.py
+++ src/leafnode.py
@@ -4,3 +4,17 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNodes must have a value.")
+
+        if self.tag is None:
+            return self.value
+
+        combined_props = ""
+        if self.props is not None:
+            for k, v in self.props.items():
+                combined_props += f' {k}="{v}"'
+
+        return f"<{self.tag}{combined_props}>{self.value}</{self.tag}>"
