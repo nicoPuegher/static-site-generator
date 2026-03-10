975ec9b5b4c578e4f0e262a5c4d96b0d2dc4e833
Nicolas <nicopuegher@gmail.com>
2026-03-10 15:09:08 +0000

    Define a unit test for a h1 element for LeafNode

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -4,7 +4,9 @@ from leafnode import LeafNode
 
 
 class TestLeafNode(unittest.TestCase):
-    pass
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "This is my title", {"class": "title"})
+        self.assertEqual(node.to_html(), '<h1 class="title">This is my title</h1>')
 
 
 if __name__ == "__main__":
