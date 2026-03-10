0ea4f9558fdcd93054288c5da2d415e9518f1055
Nicolas <nicopuegher@gmail.com>
2026-03-10 15:36:40 +0000

    Define a to_html method and raise an error if tag is missing

--- src/parentnode.py
+++ src/parentnode.py
@@ -4,3 +4,7 @@ from htmlnode import HTMLNode
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag.")
