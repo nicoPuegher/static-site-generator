29de40a2e4e317553f5f03ece792bb8ba4e546fb
Nicolas <nicopuegher@gmail.com>
2026-03-12 19:11:23 +0000

    Define a unit test for extracting markdown images

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,11 @@
+import unittest
+
+from extract_markdown import extract_markdown_images
+
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
