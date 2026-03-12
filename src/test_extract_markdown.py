8a2353c8e88d96be22f6ce958ebcbeb87be99000
Nicolas <nicopuegher@gmail.com>
2026-03-12 19:11:42 +0000

    Define a unit test for extracting markdown links

--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from extract_markdown import extract_markdown_images
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 
 class TestExtractMarkdown(unittest.TestCase):
@@ -9,3 +9,15 @@ class TestExtractMarkdown(unittest.TestCase):
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
