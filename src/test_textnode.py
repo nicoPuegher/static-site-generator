f1a191e2ed08b465eea24f3a8464b9f5e4fe033c
Nicolas <nicopuegher@gmail.com>
2026-03-12 16:01:07 +0000

    Define a unit test for bold text TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -34,6 +34,12 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_text_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
 
 if __name__ == "__main__":
     unittest.main()
