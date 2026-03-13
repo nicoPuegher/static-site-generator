a2de6e2a501c30b6f7568f8977c5d501fed86169
Nicolas <nicopuegher@gmail.com>
2026-03-13 17:32:58 +0000

    Define a unit test for splitting images at the end

--- src/test_split_nodes_images.py
+++ src/test_split_nodes_images.py
@@ -55,6 +55,22 @@ class TestSplitNodesImages(unittest.TestCase):
             new_nodes,
         )
 
+    def test_split_image_at_end(self):
+        node = TextNode(
+            "See ![logo](logo.png)",
+            TextType.TEXT,
+        )
+
+        new_nodes = split_nodes_image([node])
+
+        self.assertEqual(
+            [
+                TextNode("See ", TextType.TEXT),
+                TextNode("logo", TextType.IMAGE, "logo.png"),
+            ],
+            new_nodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
