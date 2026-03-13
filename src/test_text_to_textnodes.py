d0aa86951c32135467365132f069898f343ac8ee
Nicolas <nicopuegher@gmail.com>
2026-03-13 17:44:26 +0000

    Define a unit test to test the conversion from full markdown text

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,32 @@
+import unittest
+from textnode import TextNode, TextType
+from text_to_textnodes import text_to_textnodes
+
+
+class TestTextToTextNodes(unittest.TestCase):
+
+    def test_full_markdown(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        nodes = text_to_textnodes(text)
+
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+
+        self.assertEqual(nodes, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
