2dd126851295a3a82b5771509d615a550d43b251
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:49:36 +0000

    Define a unit test for extracting titles with spaces

--- src/test_extract_title.py
+++ src/test_extract_title.py
@@ -7,6 +7,10 @@ class TestExtractTitle(unittest.TestCase):
         md = "# Hello"
         self.assertEqual(extract_title(md), "Hello")
 
+    def test_extract_title_with_spaces(self):
+        md = "#    Hello World   "
+        self.assertEqual(extract_title(md), "Hello World")
+
 
 if __name__ == "__main__":
     unittest.main()
