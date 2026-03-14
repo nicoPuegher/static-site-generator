297fe0de953a3c269db1b00cbb04456c990e2c03
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:45:01 +0000

    Define copy_recursive fn in main

--- src/main.py
+++ src/main.py
@@ -6,4 +6,22 @@ def main():
     print(node)
 
 
-main()
+def copy_recursive(src, dest):
+    items = os.listdir(src)
+
+    for item in items:
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} -> {dest_path}")
+            shutil.copy(src_path, dest_path)
+
+        else:
+            print(f"Creating directory: {dest_path}")
+            os.mkdir(dest_path)
+            copy_recursive(src_path, dest_path)
+
+
+if __name__ == "__main__":
+    main()
