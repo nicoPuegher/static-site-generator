df137a3634a587d433356f28b02ed939e4dbbbc6
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:48:24 +0000

    Define extract_title logic

---
+++ src/extract_title.py
@@ -0,0 +1,8 @@
+def extract_title(markdown):
+    lines = markdown.split("\n")
+
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:].strip()
+
+    raise Exception("No H1 header found")
