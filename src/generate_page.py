e6dfbea89c150904e340f51da373951128baed0a
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:53:40 +0000

    Define generate_page logic

---
+++ src/generate_page.py
@@ -0,0 +1,28 @@
+import os
+from markdown_to_htmlnode import markdown_to_html_node
+from extract_title import extract_title
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path) as f:
+        markdown = f.read()
+
+    with open(template_path) as f:
+        template = f.read()
+
+    html_node = markdown_to_html_node(markdown)
+    html_content = html_node.to_html()
+
+    title = extract_title(markdown)
+
+    html_page = template.replace("{{ Title }}", title)
+    html_page = html_page.replace("{{ Content }}", html_content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w") as f:
+        f.write(html_page)
