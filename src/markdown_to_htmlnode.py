97d7289f75aafeb1ad96bbb75edbb1a06ce51ef7
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:33:52 +0000

    Define a _block_type helper fn in markdown_to_htmlnode

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -11,6 +11,25 @@ def _text_to_children(text):
     return [text_node_to_html_node(node) for node in text_nodes]
 
 
+def _block_type(block):
+    if block.startswith("#"):
+        return "heading"
+
+    if block.startswith("```") and block.endswith("```"):
+        return "code"
+
+    if block.startswith(">"):
+        return "quote"
+
+    if block.startswith("- "):
+        return "unordered_list"
+
+    if _is_ordered_list(block):
+        return "ordered_list"
+
+    return "paragraph"
+
+
 def _is_ordered_list(block):
     lines = block.split("\n")
     for i, line in enumerate(lines, start=1):
