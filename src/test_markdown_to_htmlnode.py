d723b2e103c7272e1ef3e363d77fe3a608ed2342
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:39:20 +0000

    Define a unit test for transforming into headings

--- src/test_markdown_to_htmlnode.py
+++ src/test_markdown_to_htmlnode.py
@@ -39,6 +39,21 @@ class TestMarkdownToHTML(unittest.TestCase):
             "</code></pre></div>",
         )
 
+    def test_headings(self):
+        md = "# Heading 1\n\n## Heading 2\n\n### Heading 3"
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div>"
+            "<h1>Heading 1</h1>"
+            "<h2>Heading 2</h2>"
+            "<h3>Heading 3</h3>"
+            "</div>",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
