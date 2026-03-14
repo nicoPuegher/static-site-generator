1f4aae3faf91398ace0864d0a9b5fd53f176be58
Nicolas <nicopuegher@gmail.com>
2026-03-14 12:33:28 +0000

    Define a _is_ordered_list helper fn in markdown_to_htmlnode

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -9,3 +9,11 @@ def markdown_to_html_node(markdown):
 def _text_to_children(text):
     text_nodes = text_to_textnodes(text)
     return [text_node_to_html_node(node) for node in text_nodes]
+
+
+def _is_ordered_list(block):
+    lines = block.split("\n")
+    for i, line in enumerate(lines, start=1):
+        if not line.startswith(f"{i}. "):
+            return False
+    return True
