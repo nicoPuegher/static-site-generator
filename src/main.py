a47ab8d6e714feb203e9ebf7de1e3fed0b4c92af
Nicolas <nicopuegher@gmail.com>
2026-03-16 17:43:29 +0000

    Update main.py with updated fns

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 import os
 import shutil
+import sys
 
 from generate_page import generate_page
 
 
 def main():
-    print("Copying static files to public directory...")
-    copy_static("static", "public")
+    basepath = "/"
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print("Copying static files...")
+    copy_static("static", "docs")
+
+    print("Generating pages...")
+
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 def copy_static(src, dest):
@@ -35,6 +43,26 @@ def copy_recursive(src, dest):
             print(f"Creating directory: {dest_path}")
             os.mkdir(dest_path)
             copy_recursive(src_path, dest_path)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    entries = os.listdir(dir_path_content)
+
+    for entry in entries:
+        src_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry)
+
+        if os.path.isfile(src_path):
+
+            if src_path.endswith(".md"):
+                dest_html = dest_path.replace(".md", ".html")
+
+                generate_page(src_path, template_path, dest_html, basepath)
+
+        else:
+            os.makedirs(dest_path, exist_ok=True)
+
+            generate_pages_recursive(src_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
